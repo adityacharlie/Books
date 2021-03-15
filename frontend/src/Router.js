3450c3e9327b4c9b33d67e23cd76d02353a880a4
adityakotakonda <adityakotakonda@gmail.com>
2021-03-15 12:31:12 -0400

    ran prettier on react files

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import BookScreen from './screens/BookScreen'
-import AddBookScreen from './screens/AddBookScreen/AddBookScreen'
-import DeleteBookScreen from './screens/DeleteBookScreen'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { toast, ToastContainer } from 'react-toastify'
-import 'antd/dist/antd.css';
+import React from "react";
+import BookScreen from "./screens/BookScreen";
+import AddEditBookScreen from "./screens/AddEditBookScreen/AddEditBookScreen";
+import DeleteBookScreen from "./screens/DeleteBookScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "antd/dist/antd.css";
 
+const AppRouter = (props) => {
+  return (
+    <Router>
+      <div>
+        <ToastContainer
+          hideProgressBar={true}
+          autoClose={2500}
+          position={toast.POSITION.BOTTOM_LEFT}
+        />
+        <Route path="/" exact component={BookScreen} />
+        <Route path="/addbook" exact component={AddEditBookScreen} />
+        <Route path="/editbook/:id" exact component={AddEditBookScreen} />
+        <Route path="/deletebook/:id" exact component={DeleteBookScreen} />
+      </div>
+    </Router>
+  );
+};
 
-
-const AppRouter = props => {
-    return (
-        <Router>
-        	<div>
-        		<ToastContainer hideProgressBar={true} autoClose={2500} position={toast.POSITION.BOTTOM_LEFT} />
-        		<Route path="/" exact component={BookScreen} />
-        		<Route path="/addbook" exact component={AddBookScreen} />
-        		<Route path="/editbook/:id" exact component={AddBookScreen} />
-        		<Route path="/deletebook/:id" exact component={DeleteBookScreen} />
-        	</div>
-        </Router>
-    )
-}
-
-
-export default AppRouter
+export default AppRouter;
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,33 +1,28 @@
-import { PureComponent } from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import './Navbar.css';
-import { Link } from 'react-router-dom'
+import { PureComponent } from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const { Header, Content } = Layout;
 
 class Navbar extends PureComponent {
-
   render() {
-
     return (
       <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1"><Link to={`/`}>Home</Link></Menu.Item>
-        
-        {/*<Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>*/}
-      </Menu>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-      </Breadcrumb>
-      
-    </Content>
-  </Layout>
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">
+              <Link to={`/`}>Home</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
+        </Content>
+      </Layout>
     );
   }
 }
 
-export default Navbar
+export default Navbar;

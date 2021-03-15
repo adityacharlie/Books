e6d8c1f25f84a8f9dd02f04907f4eef219a9d674
adityakotakonda <adityakotakonda@gmail.com>
2021-03-15 16:19:08 -0400

    rearrenging books backend

--- api/models.py
+++ api/models.py
@@ -1,1 +1,17 @@
+import flask_sqlalchemy
+from datetime import datetime
 
+db = flask_sqlalchemy.SQLAlchemy()
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), index=True)
+    author = db.Column(db.String(100), index=True)
+    language = db.Column(db.String(100))
+    publisher = db.Column(db.String(100), index=True)
+    genre = db.Column(db.String(100))
+    quantity = db.Column(db.Integer)
+    created = db.Column(db.Date, default=datetime.today())
--- api/schemas.py
+++ api/schemas.py
@@ -1,1 +1,13 @@
+from .models import Book
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
+
+class BookSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Book
+        include_relationships = True
+        load_instance = True
+
+
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
